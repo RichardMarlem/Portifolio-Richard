2bb6f058108703afccea3888030c688f34a22fb7
Ricky Marlem <richardmarlem@gmail.com>
2026-09-14 13:53:28 -0300

    V2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function ativaLetra(elemento) {
     const arrTexto = elemento.innerHTML.split('');
 
@@ -11,5 +12,36 @@ function ativaLetra(elemento) {
 }
 
 const titulo = document.querySelector('.digitando');
-
 ativaLetra(titulo);
+
+
+
+const navLinks = document.querySelectorAll('#menu ul a.link');
+
+navLinks.forEach(link => {
+
+    link.addEventListener('click', function(e) {
+
+        e.preventDefault();
+
+        const target = document.querySelector(
+            this.getAttribute('href')
+        );
+
+        if (target) {
+
+            const headerHeight = document.querySelector('header').offsetHeight;
+
+            const targetPosition =
+                target.offsetTop - headerHeight - 1;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+
+        } 
+
+    });
+
+});
